9dae4a5876a08095cb998295edb7112c0f5f0b53
Asha Maran <aam8mf@virginia.edu>
2021-03-28 18:45:32 -0400

    did mapBox integration tutorial

--- map_page/urls.py
+++ map_page/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls import url                                                                                                                           
 
 from . import views
 
 app_name = 'map_page'
 urlpatterns = [
     path('', views.map_page, name='map_page'),
+    url(r'', views.default_map, name="default"),
 ]
