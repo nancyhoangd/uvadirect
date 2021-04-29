b1407dc66d94cdb151dc44d0e61dd648c52b70e5
a <asd>
2021-04-29 13:28:24 -0400

    >

--- mapsite/settings.py
+++ mapsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e*)ewv(j0*^dhrhv3(^cb61k^8uq9wjdhmbv%rz+z+_qbd+5#)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
